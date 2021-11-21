324532f4f2e8338328d5725751a58f5045f50f1b
Sergio Jose Santos Junior <hagare@gmail.com>
2021-11-21 16:24:31 -0300

    Ajustada form do template Entrada

--- entrada/views.py
+++ entrada/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from estoque.models import Produto, Fornecedor
 
 def entrada(request):
     produtos = Produto.objects.all()
     fornecedores = Fornecedor.objects.all()
+
+    if request.method == 'POST':
+        # produto = Produto()
+        print(request.POST)
+        return redirect('entrada')
+
     return render(request, 'entrada/entrada.html', {'produtos': produtos, 'fornecedores': fornecedores})
