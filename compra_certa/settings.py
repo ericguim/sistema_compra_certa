6347f79250fb5ccdc1c674b1c2ee4617a6a3e0d1
Sergio Jose Santos Junior <hagare@gmail.com>
2021-11-22 18:16:22 -0300

    .

--- compra_certa/settings.py
+++ compra_certa/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['compracerta.pythonanywhere.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
     'estoque',
     'entrada',
     'saida',
