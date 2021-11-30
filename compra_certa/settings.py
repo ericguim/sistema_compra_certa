294be5843c67e4e014e1f0d1e31ccf3d98c81544
Sergio Jose dos Santos Junior <hagare@gmail.com>
2021-11-30 15:21:19 -0300

    trying another static method

--- compra_certa/settings.py
+++ compra_certa/settings.py
@@ -132,7 +132,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
